6784267a7cdd0d19997bad314953ae67bbbc3ee0
Dat <todat2001@gmail.com>
2022-09-08 13:16:56 +0700

    add landing to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import React from "react";
 import "./App.css";
+import Landing from "./Components/Header/Landing/Landing";
 import Nav from "./Components/Header/Navigation/Nav";
 
 function App() {
-  return <Nav />;
+  return (
+    <React.Fragment>
+      <Nav />
+      <Landing />
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ src/Components/Header/Landing/Landing.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./Landing.module.css";
+
+const Landing = () => {
+  return (
+    <div className={classes["header-container"]}>
+      <h1 className={classes.header}>Creative Innovative Design Agency</h1>
+      <p className={classes["header-content"]}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+        dolor sit amet, consectetur adipiscing elit.Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit.Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit.
+      </p>
+    </div>
+  );
+};
+export default Landing;
