6af02284f6b89312c426e7d892e44981463b5b1f
Dat <todat2001@gmail.com>
2022-09-09 16:57:53 +0700

    add mobile design to header nav

--- src/Components/Header/Navigation/Nav.js
+++ src/Components/Header/Navigation/Nav.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Nav.module.css";
 
 const Nav = () => {
+  const [mobileMenuIsClick, setMobileMenuIsClick] = useState(false);
+  const onClickHandler = () => {
+    setMobileMenuIsClick(true);
+  };
+  const onCloseHandler = () => {
+    setMobileMenuIsClick(false);
+  };
   return (
     <nav className={classes["nav-container"]}>
       <div className={classes["nav-logo"]}>logo</div>
+
       <div className={classes["nav-list"]}>
         <div className={classes["nav-item"]}>Work</div>
         <div className={classes["nav-item"]}>services</div>
         <div className={classes["nav-item"]}>contact</div>
+      </div>
+      <div className={classes.menu}>
+        <div
+          tabIndex={0}
+          className={classes["menu-icon"]}
+          onClick={onClickHandler}
+          onBlur={onCloseHandler}
+        >
+          |||
+        </div>
+        {mobileMenuIsClick && (
+          <div className={classes["menu-list"]}>
+            <div className={classes["menu-item"]}>Work</div>
+            <div className={classes["menu-item"]}>services</div>
+            <div className={classes["menu-item"]}>contact</div>
+          </div>
+        )}
       </div>
     </nav>
   );
