1143d3e1fd9246aa2d790623bc8b3c61a9076d4b
Dat <todat2001@gmail.com>
2022-09-09 17:48:45 +0700

    add line break between projectItems

--- src/Components/Body/Projects/Project.js
+++ src/Components/Body/Projects/Project.js
@@ -15,10 +15,26 @@ const Project = () => {
       <h2 className={classes.header}>Work</h2>
       <div className={classes["project-container"]}>
         <ProjectItem src={img1} />
+        <div className={classes.separate}>
+          <span className={classes.line}>-</span>
+        </div>
+
         <ProjectItem src={img2} />
+        <div className={classes.separate}>
+          <span className={classes.line}>-</span>
+        </div>
         <ProjectItem src={img4} />
+        <div className={classes.separate}>
+          <span className={classes.line}>-</span>
+        </div>
         <ProjectItem src={img3} />
+        <div className={classes.separate}>
+          <span className={classes.line}>-</span>
+        </div>
         <ProjectItem src={img6} />
+        <div className={classes.separate}>
+          <span className={classes.line}>-</span>
+        </div>
         <ProjectItem src={img5} />
       </div>
     </div>
